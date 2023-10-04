6a11a4c6cf4c4492d7224018ef2c0ce42f7b9f40
Capoongt <141812034+Capoongt@users.noreply.github.com>
2023-10-04 13:37:53 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+# IndexPage.vue
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+<InputPage/>
+<FunctionPage/>
+</template>
+<script>
+
+
+import InputPage from 'src/pages/InputPage.vue';
+import FunctionPage from 'src/pages/FunctionPage.vue';
+
+
+export default {
+  name: 'HtmlComponent',
+  components: {
+    InputPage,
+    FunctionPage
+  },  
+  data() {
+    return {
+      title: 'ภัสนุพงค์ แสนศักดิ์หาญ 6504101368.',
+    }
+  },
+  components: { FunctionPage,InputPage}
+
+}
+</script>
+
+<style scoped></style>
